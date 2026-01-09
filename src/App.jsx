7bc006ec91b01c9cd20b85756845b044abeef59e
Debarshi Polley <debarshipolley99@gmail.com>
2026-01-09 02:25:16 +0000

    Testimonials section (dummy for now)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from "./sections/About"
 import Projects from "./sections/Projects"
 import Experience from "./sections/Experience"
 import Testimonials from "./sections/Testimonials"
-import Contact from "./sections/Testimonials"
+import Contact from "./sections/Contact"
 import Footer from "./layout/Footer"
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <About />
         <Projects />
         <Experience />
-        <Testimonials />
+        {/* <Testimonials /> */}
         <Contact />
       </main>
       <Footer />
