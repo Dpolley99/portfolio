912b5b547cb65ec7d70c920f9ba8a64b1c04e9c9
Debarshi Polley <debarshipolley99@gmail.com>
2026-01-09 18:21:07 +0000

    UI chnages - Testimonials bg glow

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -47,9 +47,7 @@ const Testimonials = () => {
   return (
     <section id="testimonials" className="py-32 relative overflow-hidden">
       <div
-        className="absolute top-1/2 left-1/2
-       w-200 h-200 bg-primary/5
-        rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"
+        className="absolute top-1/2 left-1/2 w-150 h-120 bg-primary/5 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"
       />
       <div
         className="container mx-auto 
