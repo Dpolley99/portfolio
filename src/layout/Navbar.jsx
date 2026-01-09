a5a84773ec2b61e7b9079d57e8bf2e44dc332558
Debarshi Polley <debarshipolley99@gmail.com>
2026-01-09 02:38:49 +0000

    Navbar testimonials array -_-

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -7,7 +7,7 @@ const navLinks = [
   {href: "#about", label: "About Me"},
   { href: "#projects", label: "Projects" },
   { href: "#experience", label: "Experience" },
-  // { href: "#testimonials", label: "Testimonials" },
+  { href: "#testimonials", label: "Testimonials" },
 ];
 
 const Navbar = () => {
